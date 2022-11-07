8cab5ceca8c8d192c3d898b3613066078dda4c27
ThangHNT <thanghn2503@gmail.com>
2022-11-07 16:59:36 +0700

    set up project

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
 const userRouter = require('./user.js');
-const messageRouter = require('./message.js');
-const settingRouter = require('./setting.js');
 function route(app) {
-    app.use('/', userRouter, messageRouter, settingRouter);
+    app.use('/', userRouter);
 }
 
 module.exports = route;
